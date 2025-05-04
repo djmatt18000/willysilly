7b122101bb5712f2b2d89a89a442be6d2935232a
djmatt18000 <43975438+djmatt18000@users.noreply.github.com>
2025-05-03 20:07:46 -0500

    Create willysilly.sh

---
+++ willysilly.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is a comment
+clear
+echo "Hello there!"
+read -p "What's Is your Name? " name
+echo
+echo "Nice to meet you $name!"
+echo "I do not like your name $name..."
+echo "Bye!"
